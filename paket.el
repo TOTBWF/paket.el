03db780d16f3c2f9e7a9f795d92c989d6fe4c0e0
Reed Mullanix <reed.mullanix@mybazinga.com>
2019-08-02 11:51:44 -0700

    Implement 'paket restore' and 'paket mode'

--- paket.el
+++ paket.el
@@ -17,7 +17,15 @@
 (define-suffix-command paket-install-command ()
   :transient nil
   (interactive)
-  (compile (apply 'paket-command "install" (transient-args))))
+  (compile (apply 'paket-command "install" (transient-args 'paket-install))))
+
+(define-infix-argument paket:--preserve-level ()
+  :description "Preserve"
+  :class 'transient-option
+  :key "-p"
+  :argument "--keep-"
+  :choices '("major" "minor" "patch")
+  )
 
 (define-transient-command paket-install ()
   :value '()
@@ -25,16 +33,17 @@
    ("-f" "Force" "--force")
    ("-r" "Only Referenced" "--only-referenced")
    ]
+  ["Options"
+   ("=p" paket:--preserve-level)
+   ]
   ["Actions"
    ("i" "Install" paket-install-command)]
   (interactive)
   (transient-setup 'paket-install nil nil))
 
-
-;;;###autoload
-(define-transient-command paket-pack-command (output-path)
+(define-suffix-command paket-pack-command (output-path)
   (interactive "DOutput Directory: ")
-  (compile (apply 'paket-command "pack" output-path (transient-args))))
+  (compile (apply 'paket-command "pack" output-path (transient-args 'paket-pack))))
 
 (define-transient-command paket-pack ()
   :value '()
@@ -42,23 +51,44 @@
    ("=v" "Version" "--version " read-string)
    ]
   ["Actions"
-   ("p" "Pack" paket-pack-command)]
-  (interactive)
-  (transient-setup 'paket-pack nil nil))
+   ("p" "Pack" paket-pack-command)])
 
-(define-transient-command paket ()
+(define-suffix-command paket-restore-command ()
+  (interactive)
+  (compile (apply 'paket-command "restore" (transient-args 'paket-restore))))
+
+(define-transient-command paket-restore ()
   :value '()
-  [["Update"
-    ("i" "Install" paket-install)]
-   ["Release"
-    ("p" "Pack" paket-pack)]]
-  (interactive)
-  (transient-setup 'paket nil nil))
+  ["Arguments"
+   ("-f" "Force" "--force")
+   ("-r" "Only Referenced" "--only-referenced")
+   ]
+  ["Actions"
+   ("r" "Restore" paket-restore-command)
+   ])
 
-;; (define-suffix-command paket-list-packages ()
-;;   :transient nil
-;;   (interactive)
-;;   )
+(defun paket--list-packages ()
+    (with-temp-buffer
+      (insert (shell-command-to-string (paket-command "show-installed-packages")))
+      (goto-char (point-min))
+      ))
+
+(defvar paket-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-i") 'paket-install)
+    (define-key map (kbd "C-c C-p") 'paket-pack)
+    (define-key map (kbd "C-c C-r") 'paket-restore)
+    map))
+
+(defvar paket-keywords
+  '(("^[[:blank:]]*\\(nuget\\|source\\|version\\|group\\)" (1 font-lock-keyword-face))))
+
+(add-to-list 'auto-mode-alist '("paket.dependencies" . paket-mode))
+
+(define-derived-mode paket-mode prog-mode "Paket"
+  "Major mode for editing paket files"
+  (use-local-map paket-mode-map)
+  (setq font-lock-defaults '(paket-keywords)))
 
 (provide 'paket)
 ;;; paket.el ends here
